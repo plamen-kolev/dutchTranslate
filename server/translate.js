04289727a149221bdad46bceb32169ffa5473ea7
Sam Tildon <hallagat@192.168.42.52>
2019-06-30 10:19:42 +0100

    translation server done

---
+++ server/translate.js
@@ -0,0 +1,37 @@
+const { Translate } = require('@google-cloud/translate');
+const translate = new Translate();
+
+// Imports the Google Cloud client library
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/etc/compute.json';
+
+const translateFacade = (text, from, to) => {
+  if (process.env.REAL_TRANSLATE) {
+    return realTranslate(text, from, to);
+  }
+  return fakeTranslate(text, from, to);
+}
+
+const fakeTranslate = (text, from, to) => {
+  
+  return Promise.resolve([
+      `${text} in ${to}`, {"data":
+      {
+        "translations":[{
+          "translatedText":text,
+          "model":"nmt"
+        }]
+      }}
+  ]);
+}
+
+const realTranslate = (text, from, to) => {
+  console.log("I AM COSTING YOU MONEY, Mate !");
+  const options = { format: 'text', from: from, model: 'nmt', to: to}
+  console.log("Options: ", options);
+  return translate.translate(text, options)
+};
+
+
+module.exports = {
+  translate: translateFacade
+}
